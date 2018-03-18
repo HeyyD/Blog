e11e4a25d8bdf637356523f9a7695415ccce95a2
HeyyD <heidi.hati@cs.tamk.fi>
2018-03-18 20:38:21 +0200

    Save username to state

--- front-end/src/profile/Login.js
+++ front-end/src/profile/Login.js
@@ -2,15 +2,32 @@ import React, { Component } from 'react';
 import './Login.css'
 
 class Login extends Component {
+
+  constructor(props) {
+    super(props);
+    this.login = this.login.bind(this);
+    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.state = {"username":"none"}
+  }
+
+  onUsernameChange(event){
+    this.setState({"username": event.target.value});
+  }
+
+  login(event) {
+    console.log(this.state.username);
+    event.preventDefault()
+  }
+
   render() {
     return(
-      <form className="Login-form">
+      <div className="Login-form">
         Username:
-        <input type="text"/>
+        <input type="text" onChange={this.onUsernameChange}/>
         Password:
         <input type="text"/>
-        <button>Login</button>
-      </form>
+        <button onClick={this.login}>Login</button>
+      </div>
     );
   }
 }
