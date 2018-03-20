d2ca221afce5d664411661d8cc77c61785a8db4b
HeyyD <heidi.hati@cs.tamk.fi>
2018-03-20 20:44:16 +0200

    Create blog posts from data

--- front-end/src/post-feed/PostFeed.js
+++ front-end/src/post-feed/PostFeed.js
@@ -2,28 +2,30 @@ import React, { Component } from 'react';
 import Post from './Post'
 
 class PostFeed extends Component {
-
   constructor(props) {
     super(props);
-    this.createFeed = this.createFeed.bind(this);
+    this.state = {'posts': []};
   }
 
   componentDidMount() {
     let url = 'http://localhost:8080/posts'
     fetch(url).then(result => result.json())
-              .then(res => this.createFeed(res));
-  }
-
-  createFeed(json) {
-    for(let i = 0; i < json.length; i++) {
-      console.log(json[i].title);
-    }
+              .then(res => this.setState({'posts': res}));
   }
 
   render() {
+
+    let posts = [];
+
+    for(let i = 0; i < this.state.posts.length; i++) {
+      let title = this.state.posts[i].title;
+      let content = this.state.posts[i].content;
+      posts.push(<Post title={title} content={content}/>);
+    }
+
     return(
       <div>
-        <Post title='title' content='content'/>
+        {posts}
       </div>
     );
   }
