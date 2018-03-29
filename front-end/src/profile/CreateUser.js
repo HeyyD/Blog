8aad30c4e2e1b78747f3a74b2da2a3325582a903
HeyyD <heidi.hati@cs.tamk.fi>
2018-03-29 21:00:01 +0300

    Modify CreateUser

--- front-end/src/profile/CreateUser.js
+++ front-end/src/profile/CreateUser.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 class CreateUser extends Component {
   render() {
-    return <p>Create User</p>
+    return(
+      <form className="Create-account">
+        <input type="text" placeholder="Username"/>
+        <input type="password" placeholder="Password"/>
+        <input type="password" placeholder="Confirm password"/>
+        <button>Create an account!</button>
+      </form>
+    );
   }
 }
 export default CreateUser;
