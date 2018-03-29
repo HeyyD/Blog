8f47706d30b2208c7bf1af05fcddfce2944050e8
HeyyD <heidi.hati@cs.tamk.fi>
2018-03-29 22:20:49 +0300

    handle user information changes

--- front-end/src/profile/CreateUser.js
+++ front-end/src/profile/CreateUser.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import './CreateUser.css';
 
 class CreateUser extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      username: '',
+      password: '',
+      confirm: ''
+    }
+
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
   render() {
     return(
       <form className="Create-account">
-        <input type="text" placeholder="Username"/>
-        <input type="password" placeholder="Password"/>
-        <input type="password" placeholder="Confirm password"/>
+        <input name="username" type="text" placeholder="Username" onChange={this.handleChange}/>
+        <input name="password" type="password" placeholder="Password" onChange={this.handleChange}/>
+        <input name="confirm" type="password" placeholder="Confirm password" onChange={this.handleChange}/>
         <input type="button" value="Create an account!" />
       </form>
     );
