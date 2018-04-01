d4a63455ba4604b6de9d4e41b792daf8b97f079a
HeyyD <heidi.hati@cs.tamk.fi>
2018-04-01 23:54:51 +0300

    form for creating post

--- front-end/src/post-feed/CreatePost.js
+++ front-end/src/post-feed/CreatePost.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import './CreatePost.css'
+
 
 class CreatePost extends Component {
     render() {
         return(
-            <p>Create Post</p>
+            <div className="Create-post-container">
+                <form className="Create-post">
+                    <input name="title" type="text" placeholder="Title"/>
+                    <textarea name="content" placeholder="Content"/>
+                    <input type="button" value="Post"/>
+                </form>
+            </div>
         );
     }
 }
