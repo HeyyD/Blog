a855de5bec87750ec03e3208db2694e2afdb185d
HeyyD <heidi.hati@cs.tamk.fi>
2018-04-02 19:41:29 +0300

    CreatePost save states

--- front-end/src/post-feed/CreatePost.js
+++ front-end/src/post-feed/CreatePost.js
@@ -3,12 +3,26 @@ import './CreatePost.css'
 
 
 class CreatePost extends Component {
+
+    constructor(props) {
+        super(props)
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            title: '',
+            content: ''
+        }
+    }
+
+    handleChange(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
     render() {
         return(
             <div className="Create-post-container">
                 <form className="Create-post">
-                    <input name="title" type="text" placeholder="Title"/>
-                    <textarea name="content" placeholder="Content"/>
+                    <input name="title" type="text" placeholder="Title" onChange={this.handleChange}/>
+                    <textarea name="content" placeholder="Content" onChange={this.handleChange}/>
                     <input type="button" value="Post"/>
                 </form>
             </div>
