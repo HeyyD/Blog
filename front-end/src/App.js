4d7d4aeb6cb2d301861f8fbfdd69689da2a55e0b
HeyyD <heidi.hati@cs.tamk.fi>
2018-04-03 19:57:01 +0300

    Main menu visible at start

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,32 +6,13 @@ import MainMenu from './mainmenu/MainMenu';
 
 class App extends Component {
 
-    /*
-        $(document).ready(function() {
-           $(window).on('scroll', function() {
-            if($(window).scrollTop() < 1000) {
-              $('.hero').css('background-size', 130 + parseInt($(window).scrollTop() / 5) + '%');
-              $('.hero h1').css('top', 50 + ($(window).scrollTop() * .1) + '%');
-              $('.hero h1').css('opacity', 1 - ($(window).scrollTop() * .003));
-            }
-
-             if($(window).scrollTop() >= $('.content-wrapper').offset().top - 300) {
-               $('.nav-bg').removeClass('bg-hidden');
-               $('.nav-bg').addClass('bg-visible');
-             } else {
-               $('.nav-bg').removeClass('bg-visible');
-               $('.nav-bg').addClass('bg-hidden');
-             }
-          });
-        });
-     */
-
     constructor(props) {
         super(props);
         this.handleScroll = this.handleScroll.bind(this);
     }
 
     componentDidMount() {
+        $('.nav-bg').addClass('bg-visible');
         window.addEventListener('scroll', this.handleScroll)
     }
 
@@ -43,11 +24,11 @@ class App extends Component {
         }
 
         if($(window).scrollTop() >= $('.content-wrapper').offset().top - 300) {
-            $('.nav-bg').removeClass('bg-hidden');
-            $('.nav-bg').addClass('bg-visible');
-        } else {
             $('.nav-bg').removeClass('bg-visible');
             $('.nav-bg').addClass('bg-hidden');
+        } else {
+            $('.nav-bg').removeClass('bg-hidden');
+            $('.nav-bg').addClass('bg-visible');
         }
     }
 
