801d43b047fafdc7d8fa2ec41fccb173eb549851
HeyyD <heidi.hati@cs.tamk.fi>
2018-04-06 22:39:45 +0300

    Render comments

--- front-end/src/comments/Comment.js
+++ front-end/src/comments/Comment.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 
 class Comment extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      content: ''
+    }
+  }
+
+  componentWillMount() {
+    fetch(window.location.origin + '/users/' + this.props.userId)
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          username: result.username,
+          content: this.props.content
+        });
+      })
+  }
+
   render() {
     return(
-      <p>This is a comment</p>
+      <div>
+        <h4>{this.state.username}</h4>
+        <p>{this.state.content}</p>
+      </div>
     );
   }
 }
--- front-end/src/comments/CommentFeed.js
+++ front-end/src/comments/CommentFeed.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
 import './Comment';
+import Comment from "./Comment";
 
 class CommentFeed extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: []
+    }
+  }
+
+  componentWillMount() {
+    fetch(window.location.href + '/comments')
+      .then(res => res.json())
+      .then(result => {
+        for(let c of result) {
+          this.state.comments.push(<Comment key={c.id} userId={c.userId} content={c.content}/>);
+        }
+      });
+  }
+
   render() {
     return(
-      <p>This is the comment feed</p>
+      <div>
+        {this.state.comments}
+      </div>
     );
   }
 }
