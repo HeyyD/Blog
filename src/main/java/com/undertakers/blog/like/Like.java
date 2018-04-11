5b9812df72f6cdb3409afe908bcfca7901eb9c6e
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-11 20:04:11 +0300

    Modify Like.java add attributes
    -(Like): Add attributes id, postId, userId and date

--- src/main/java/com/undertakers/blog/like/Like.java
+++ src/main/java/com/undertakers/blog/like/Like.java
@@ -7,5 +7,14 @@ import java.util.Date;
 @TableGenerator(name = "like", initialValue = 0)
 public class Like {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "like")
+    @Column(name = "id")
+    private int id;
+    @Column(name = "post_id")
+    private int postId;
+    @Column(name = "user_id")
+    private int userId;
+    @Column(name = "date")
+    private Date date;
 }
