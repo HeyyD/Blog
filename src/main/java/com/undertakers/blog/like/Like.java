e796b613560395e574aaa39d4b555202e868f671
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-11 19:58:34 +0300

    Add Like.java add new class
    -(Like): Add new class to make like objects

---
+++ src/main/java/com/undertakers/blog/like/Like.java
@@ -0,0 +1,11 @@
+package com.undertakers.blog.like;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@TableGenerator(name = "like", initialValue = 0)
+public class Like {
+
+    
+}
