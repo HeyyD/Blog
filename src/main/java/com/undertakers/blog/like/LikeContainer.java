1e92fc6161e7822734c931af3ab8048cc6ab653d
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-17 00:54:10 +0300

    Rename class Like to LikeContainer

---
+++ src/main/java/com/undertakers/blog/like/LikeContainer.java
@@ -0,0 +1,15 @@
+package com.undertakers.blog.like;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+
+public class LikeContainer {
+
+    private List<Integer> idList;
+
+    public LikeContainer() {
+
+    }
+}
