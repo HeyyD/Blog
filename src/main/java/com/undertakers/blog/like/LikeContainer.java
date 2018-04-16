7be7465413f9b93d668d9042ce675efe99fa3afe
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-17 01:39:33 +0300

    Modify LikeContainer.java add new methods
    -(constructor()): Add empty constructor method
    -(constructor(String)): Add constructor that
    initializes the id list
    -(addLike): Add method for adding new likes to list
    -(toList): Add method that converts given string to
    List<Integer>
    -(toString): Add method that converts id list to String
    -(getIdList): Add getter method for id list

--- src/main/java/com/undertakers/blog/like/LikeContainer.java
+++ src/main/java/com/undertakers/blog/like/LikeContainer.java
@@ -1,7 +1,6 @@
 package com.undertakers.blog.like;
 
-import javax.persistence.*;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -10,6 +9,43 @@ public class LikeContainer {
     private List<Integer> idList;
 
     public LikeContainer() {
+        idList = new ArrayList<>();
+    }
 
+    public LikeContainer(String idString) {
+        idList = toList(idString);
+    }
+
+    public boolean addLike(int userId) {
+        if(!idList.contains(userId)) {
+            idList.add(userId);
+        }
+
+        return !idList.contains(userId);
+    }
+
+    public List<Integer> toList(String s) {
+        List<Integer> list = new ArrayList<>();
+
+        String[] strings = s.split(":");
+        for (String number: strings) {
+            list.add(Integer.parseInt(number));
+        }
+
+        return list;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Integer i: idList) {
+            sb.append(i);
+            sb.append(':');
+        }
+        return sb.toString();
+    }
+
+    public List<Integer> getIdList() {
+        return idList;
     }
 }
