e49bc1468948c40915e7e3d00bfdfc540652ddda
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-26 08:23:03 +0300

    Modify BlogApplication.java add javadoc

--- src/main/java/com/undertakers/blog/BlogApplication.java
+++ src/main/java/com/undertakers/blog/BlogApplication.java
@@ -3,9 +3,17 @@ package com.undertakers.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class of the application.
+ */
 @SpringBootApplication
 public class BlogApplication {
 
+	/**
+	 * Starts the backend of the application.
+	 *
+	 * @param args Given command line arguments.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
