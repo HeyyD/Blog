dfe18b9bb44af64abbd53f5bc369565a90bbb8b9
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-26 08:07:43 +0300

    Modify MemberNotFoundException add javadoc

--- src/main/java/com/undertakers/blog/user/MemberNotFoundException.java
+++ src/main/java/com/undertakers/blog/user/MemberNotFoundException.java
@@ -1,10 +1,26 @@
 package com.undertakers.blog.user;
 
+/**
+ * Exception that is thrown when a specified user cannot be found.
+ */
 public class MemberNotFoundException extends IllegalArgumentException {
+
+    /**
+     * Id of the exception.
+     */
     private int id;
 
+    /**
+     * Constructs the exception and sets its id.
+     *
+     * @param id Id of the exception.
+     */
     public MemberNotFoundException(int id) {
         this.id = id;
     }
+
+    /**
+     * @return Id of the exception.
+     */
     public int getId(){return this.id;}
 }
